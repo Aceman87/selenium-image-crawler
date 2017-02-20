129813f13deae12a38106b86e14dd6a9fa50669b
Aceman87 <juho.suominen@gmail.com>
2017-02-21 00:17:00 +0200

    Added a new Yandex example

---
+++ example_yandex2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from crawler.YandexCrawler import YandexCrawler
+from processor.LogProcessor import LogProcessor
+from processor.DownloadProcessor import DownloadProcessor
+
+if __name__ == '__main__':
+
+    options = {
+        # defines the directory where the images and the log will be saved
+        'output_directory':  "./images"
+    }
+    w = YandexCrawler(search_key='Apple')
+    # w.append_processor(LogProcessor())
+    w.append_processor(DownloadProcessor(output_directory=options['output_directory'], process_count=16))
+    w.run()
